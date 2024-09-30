1960ea189a6cd3b30eae7e2820abc8261ff0c84f
Hantao Cui <cuihantao@gmail.com>
2024-09-30 12:30:36 -0400

    Update to use `-t gfm`

--- pct/modules/01/ipynb_to_md.sh
+++ pct/modules/01/ipynb_to_md.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pandoc _git.ipynb -o git.md --extract-media=./media
-pandoc _jupyter.ipynb -o jupyter.md --extract-media=./media
-pandoc _env.ipynb -o env.md --extract-media=./media
+pandoc _git.ipynb -o git.md -t gfm --extract-media=./media
+pandoc _jupyter.ipynb -o jupyter.md -t gfm --extract-media=./media
+pandoc _env.ipynb -o env.md -t gfm --extract-media=./media
